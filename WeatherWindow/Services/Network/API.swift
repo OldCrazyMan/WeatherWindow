e5b5d2a600f3674e9937af9138cfa90ddea41513
Tim Akhm <akhmtimyr@gmail.com>
2022-12-08 18:49:13 +0200

    Security key

--- WeatherWindow/Services/Network/API.swift
+++ WeatherWindow/Services/Network/API.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct API {
     
-    static let apiKey = "926247ac53ed30b659cab42495a479fd"
+    static let apiKey = "Use your key"
     static let url =
     "https://api.openweathermap.org/data/2.5/onecall?exclude=minutely&lang=ru&units=metric&appid=926247ac53ed30b659cab42495a479fd&"
 }
